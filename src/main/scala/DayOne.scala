b76300c52b02509fcdf6041a89bea8a61526494d
Sameer Brenn <sbrenn@twitter.com>
2020-12-05 15:18:06 -0700

    day 1 part 1

---
+++ src/main/scala/DayOne.scala
@@ -0,0 +1,26 @@
+import scala.io.Source
+import scala.util.Try
+
+object DayOne extends App {
+  def findSum(input: Seq[Long]): Try[Long] = {
+    val maybeResult: Either[Set[Long], Long] = input.foldLeft[Either[Set[Long], Long]](Left(Set())) {
+      case (Left(set), value) => if (set.contains(2020 - value)) {
+        Right(value * (2020 - value))
+      } else {
+        Left(set + value)
+      }
+      case (Right(result), _) =>
+        Right(result)
+    }
+
+    val leftException: Either[RuntimeException, Long] = maybeResult.left.map { _ =>
+      new RuntimeException("No valid result")
+    }
+
+    leftException.toTry
+  }
+
+  val source = Source.fromFile("src/main/scala/day1.txt").getLines()
+  val input = source.toSeq.map(_.toLong)
+  println(findSum(input).get)
+}
