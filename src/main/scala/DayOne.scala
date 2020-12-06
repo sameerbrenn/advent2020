e1b09a86194d73bfb58acf0ee96e2811e0a35e13
Sameer Brenn <sbrenn@twitter.com>
2020-12-06 15:02:17 -0700

    avoid match

--- src/main/scala/DayOne.scala
+++ src/main/scala/DayOne.scala
@@ -4,13 +4,13 @@ import scala.util.Try
 object DayOne extends App {
   def findSum(input: Seq[Long]): Try[Long] = {
     val maybeResult: Either[Set[Long], Long] = input.foldLeft[Either[Set[Long], Long]](Left(Set())) {
-      case (Left(set), value) => if (set.contains(2020 - value)) {
-        Right(value * (2020 - value))
-      } else {
-        Left(set + value)
+      case (maybeSet, value) => maybeSet.left.flatMap { set =>
+        if (set.contains(2020 - value)) {
+          Right(value * (2020 - value))
+        } else {
+          Left(set + value)
+        }
       }
-      case (Right(result), _) =>
-        Right(result)
     }
 
     val leftException: Either[RuntimeException, Long] = maybeResult.left.map { _ =>
